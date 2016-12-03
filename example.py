0cd33b9a4bca3adcc2793bea28599f465e85a8c7
Mark Hubenthal <markhubenthal@gmail.com>
2016-12-03 08:55:04 -0600

    flush

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 from scrapediscogs import MusicDatabase
 
 music_db = MusicDatabase('test.db', 85)
-music_db.add_data('albumsinput.txt')
+#music_db.add_data('albumsinput.txt')
 
-r = music_db.find_albums_by_artist('David Sanborn')
-print len(r)
-print r[-1]
+#r = music_db.find_albums_by_artist('David Sanborn')
+#print len(r)
+#print r[-1]
 
-for r in music_db.find_album(album_title = "The Call"):
-	print r
+#for r in music_db.find_album(album_title = "The Call"):
+#	print r
 
 for r in music_db.find_songs_by_album_artist('Seamus'):
 	print r
